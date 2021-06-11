a01252c71a5ce50319997eb27b444dcf683db65a
fenn-cs <fenn25.fn@gmail.com>
2021-06-11 19:01:14 +0100

    initial: scrum-ui added
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

---
+++ scrum-ui/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Scrum App</h1>
+    <div class="jumbotron">
+      Welcome to the srum tracking application.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ scrum-ui/pages/users.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Users</h1>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
