77d5a0e865220c475ee920efb9e79c5ce683b48b
henry247 <henrykonforft@gmail.com>
2021-06-28 16:19:02 +0100

    added delete standup

--- Controllers/StandUpController.js
+++ Controllers/StandUpController.js
@@ -1,149 +1,210 @@
-import StandUp from '../Models/StandUp.js'
-import StandUpUpdate from '../Models/StandUpUpdate.js'
-import User from '../Models/User.js'
-import { validationResult } from 'express-validator';
+import StandUp from "../Models/StandUp.js";
+import StandUpUpdate from "../Models/StandUpUpdate.js";
+import User from "../Models/User.js";
+import { validationResult } from "express-validator";
 
 class StandUpController {
-    
-    createNewStandUp (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        StandUp.create(req.body.standup).then(standup => {
-            res.json({ standup });
-        }).catch((error) => {
-            res.json({ standup: { message: "There was an error creating new StandUp", errorDetails: error } });
+  createNewStandUp(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    StandUp.create(req.body.standup)
+      .then((standup) => {
+        res.json({ standup });
+      })
+      .catch((error) => {
+        res.json({
+          standup: {
+            message: "There was an error creating new StandUp",
+            errorDetails: error,
+          },
         });
+      });
+  }
 
+  async deleteStandUp(req, res) {
+    let id = req.params.id;
+    StandUp.findByIdAndDelete(id)
+      .then((deleteStandUp) => {
+        res.json({ deleteStandUp: "successfully deleted" });
+      })
+      .catch((error) => {
+        res.status(404).json({ mesage: " error deleting standup" });
+      });
+  }
+
+  async standUpList(req, res) {
+    let standUps = await StandUp.find();
+    res.json({ standups: standUps });
+  }
+
+  async standUpResponses(req, res) {
+    let responses = null;
+    if (req.query.standup_id) {
+      responses = await StandUpUpdate.find({
+        standup_id: req.query.standup_id,
+      });
+      res.json({ standUpResponses: responses });
     }
 
-    deleteStandUp (req, res) {
+    responses = await StandUpUpdate.find();
+    res.json({ standUpResponses: responses });
+  }
 
+  async updateStandUp(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    async standUpList (req, res) {
-        let standUps = await StandUp.find();
-        res.json({ 'standups': standUps });
-
+    try {
+      var standUp = await StandUp.findOne({ _id: req.body.standup.id });
+    } catch (e) {
+      return res
+        .status(404)
+        .json({ message: "StandUp not found!", errors: e.message });
     }
 
-    async standUpResponses (req, res) {
-        let responses = null;
-        if(req.query.standup_id) {
-            responses = await StandUpUpdate.find({"standup_id": req.query.standup_id });
-            res.json({ standUpResponses: responses });
-        }
-
-        responses = await StandUpUpdate.find();
-        res.json({ standUpResponses: responses });
-
+    try {
+      standUp = await StandUp.findOneAndUpdate(
+        { _id: standUp.id },
+        req.body.standup,
+        { new: true, runValidators: true }
+      );
+      // StandUp.findOneAndUpdate()
+    } catch (e) {
+      return res
+        .status(404)
+        .json({ message: "Error updating standup!", errors: e.message });
     }
 
-    async updateStandUp (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+    res.json({ StandUp: standUp });
+  }
 
-        try {
-            var standUp = await StandUp.findOne({ _id: req.body.standup.id });
-        } catch (e) {
-            return res.status(404).json({ message: "StandUp not found!", errors: e.message });
-        }
-
-        try {
-            standUp = await StandUp.findOneAndUpdate({ _id: standUp.id }, req.body.standup, {new: true, runValidators: true});
-            // StandUp.findOneAndUpdate()
-        } catch (e) {
-            return res.status(404).json({ message: "Error updating standup!", errors: e.message });
-        }
-
-        res.json({ 'StandUp': standUp });
+  async completeStandUp(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    async completeStandUp (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        StandUpUpdate.create(req.body.standup_update).then((standUpUpdate) => {
-            res.json({ 'standUpUpdate': standUpUpdate });
-        }).catch((error) => {
-            res.json({ StandUp: { message: "There was an error creating new StandUp", errorDetails: error } });
+    StandUpUpdate.create(req.body.standup_update)
+      .then((standUpUpdate) => {
+        res.json({ standUpUpdate: standUpUpdate });
+      })
+      .catch((error) => {
+        res.json({
+          StandUp: {
+            message: "There was an error creating new StandUp",
+            errorDetails: error,
+          },
         });
-        
-       
+      });
+  }
+
+  async subscribeToStandUp(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    async subscribeToStandUp (req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        try {
-            var standUp = await StandUp.findOne({ _id: req.body.standup_id });
-        } catch (e) {
-            return res.status(404).json({ message: "StandUp not found!", errors: e.message });
-        }
-
-        try {
-            var user = await User.findOne({ username: req.user.username });
-        } catch (e) {
-            return res.status(500).json({ message: "Error getting user", errors: e.message });
-        }
-
-        if (user.standups.length > 0) {
-            if (user.standups.indexOf(standUp.id) > -1) {
-                return res.status(409).json({ 'success': false, message: "User already subscribed", user: user });
-            }
-        }
-
-        try {
-            user.standups.push(standUp.id);
-            user = await User.findOneAndUpdate({ username: user.username }, {$set:{standups : user.standups}}, {new: true});
-        } catch (e) {
-            return res.status(404).json({ message: "Error subscribing user to standup!", errors: e.message });
-        }
-
-        return res.json({ 'success': true, user: user });
-
+    try {
+      var standUp = await StandUp.findOne({ _id: req.body.standup_id });
+    } catch (e) {
+      return res
+        .status(404)
+        .json({ message: "StandUp not found!", errors: e.message });
     }
 
-    async unsubscribeToStandUp (req, res) {
-
-        try {
-            var standUp = await StandUp.findOne({ _id: req.body.standup_id });
-        } catch (e) {
-            return res.status(404).json({ message: "StandUp not found!", errors: e.message });
-        }
-
-        try {
-            var user = await User.findOne({ username: req.user.username });
-        } catch (e) {
-            return res.status(500).json({ message: "Error getting user", errors: e.message });
-        }
-
-
-        if (user.standups.length > 0) {
-            var scrumIndex = user.standups.indexOf(standUp.id);
-            if (scrumIndex === -1) {
-                return res.status(409).json({ 'success': false, message: "User not subscribed to scrum", user: user });
-            } 
-        }
-
-        try {
-            user.standups = user.standups.slice(0,scrumIndex).concat(user.standups.slice(scrumIndex+1))
-            user = await User.findOneAndUpdate({ username: user.username }, {$set:{standups : user.standups}}, {new: true});
-        } catch (e) {
-            return res.status(404).json({ message: "Error subscribing user to standup!", errors: e.message });
-        }
-
-        return res.json({ 'success': true, user: user });
-
+    try {
+      var user = await User.findOne({ username: req.user.username });
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: "Error getting user", errors: e.message });
     }
+
+    if (user.standups.length > 0) {
+      if (user.standups.indexOf(standUp.id) > -1) {
+        return res
+          .status(409)
+          .json({
+            success: false,
+            message: "User already subscribed",
+            user: user,
+          });
+      }
+    }
+
+    try {
+      user.standups.push(standUp.id);
+      user = await User.findOneAndUpdate(
+        { username: user.username },
+        { $set: { standups: user.standups } },
+        { new: true }
+      );
+    } catch (e) {
+      return res
+        .status(404)
+        .json({
+          message: "Error subscribing user to standup!",
+          errors: e.message,
+        });
+    }
+
+    return res.json({ success: true, user: user });
+  }
+
+  async unsubscribeToStandUp(req, res) {
+    try {
+      var standUp = await StandUp.findOne({ _id: req.body.standup_id });
+    } catch (e) {
+      return res
+        .status(404)
+        .json({ message: "StandUp not found!", errors: e.message });
+    }
+
+    try {
+      var user = await User.findOne({ username: req.user.username });
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: "Error getting user", errors: e.message });
+    }
+
+    if (user.standups.length > 0) {
+      var scrumIndex = user.standups.indexOf(standUp.id);
+      if (scrumIndex === -1) {
+        return res
+          .status(409)
+          .json({
+            success: false,
+            message: "User not subscribed to scrum",
+            user: user,
+          });
+      }
+    }
+
+    try {
+      user.standups = user.standups
+        .slice(0, scrumIndex)
+        .concat(user.standups.slice(scrumIndex + 1));
+      user = await User.findOneAndUpdate(
+        { username: user.username },
+        { $set: { standups: user.standups } },
+        { new: true }
+      );
+    } catch (e) {
+      return res
+        .status(404)
+        .json({
+          message: "Error subscribing user to standup!",
+          errors: e.message,
+        });
+    }
+
+    return res.json({ success: true, user: user });
+  }
 }
 
 export default new StandUpController();
--- routes/web.js
+++ routes/web.js
@@ -1,50 +1,65 @@
+import { Router } from "express";
+import publicController from "../Controllers/PublicController.js";
+import standUpController from "../Controllers/StandUpController.js";
+import userController from "../Controllers/UserController.js";
+import authenticationController from "../Controllers/AuthenticationController.js";
+import auth from "../Middlewares/AuthMiddleware.js";
+import { body } from "express-validator";
 
-import { Router } from 'express'
-import publicController from '../Controllers/PublicController.js'
-import standUpController from '../Controllers/StandUpController.js'
-import userController from '../Controllers/UserController.js'
-import authenticationController from '../Controllers/AuthenticationController.js'
-import auth from '../Middlewares/AuthMiddleware.js'
-import { body } from 'express-validator'
+const router = Router();
+const authMiddleware = auth.authenticate("basic", { session: false });
 
-const router = Router()
-const authMiddleware = auth.authenticate('basic', { session: false });
-
-
-router.get('/', publicController.welcome);
-router.post('/standups/new', [
-    body('standup').isObject(),
-    authMiddleware
-], standUpController.createNewStandUp);
-router.post('/standups/delete', authMiddleware, standUpController.deleteStandUp);
-router.get('/standups', authMiddleware, standUpController.standUpList);
-router.get('/standups/responses', authMiddleware, standUpController.standUpResponses);
-router.post('/standups/subcribe', [
-    body('standup_id').isString(),
-    authMiddleware
-], standUpController.subscribeToStandUp);
-router.post('/standups/unsubcribe', [
-    body('standup_id').isString(),
-    authMiddleware
-], standUpController.unsubscribeToStandUp);
-router.post('/standups/update', [
-    body('standup').isObject(),
-    authMiddleware
-], standUpController.updateStandUp);
-router.post('/standups/complete', [
-    body('standup_update').isObject(),
-    authMiddleware
-], standUpController.completeStandUp);
-router.get('/users', authMiddleware, userController.users);
+router.get("/", publicController.welcome);
+router.post(
+  "/standups/new",
+  [body("standup").isObject(), authMiddleware],
+  standUpController.createNewStandUp
+);
+router.post(
+  "/standups/delete/:id",
+  authMiddleware,
+  standUpController.deleteStandUp
+);
+router.get("/standups", authMiddleware, standUpController.standUpList);
+router.get(
+  "/standups/responses",
+  authMiddleware,
+  standUpController.standUpResponses
+);
+router.post(
+  "/standups/subcribe",
+  [body("standup_id").isString(), authMiddleware],
+  standUpController.subscribeToStandUp
+);
+router.post(
+  "/standups/unsubcribe",
+  [body("standup_id").isString(), authMiddleware],
+  standUpController.unsubscribeToStandUp
+);
+router.post(
+  "/standups/update",
+  [body("standup").isObject(), authMiddleware],
+  standUpController.updateStandUp
+);
+router.post(
+  "/standups/complete",
+  [body("standup_update").isObject(), authMiddleware],
+  standUpController.completeStandUp
+);
+router.get("/users", authMiddleware, userController.users);
 
 //Authentication routes
 
-router.post('/login', authMiddleware, authenticationController.login);
-router.post('/users/register', [
-    body('user').isObject(),
-    body('user.username').isString(),
-    body('user.email').isEmail(),
-    body('user.password').isString(),
-], authenticationController.createUser);
+router.post("/login", authMiddleware, authenticationController.login);
+router.post(
+  "/users/register",
+  [
+    body("user").isObject(),
+    body("user.username").isString(),
+    body("user.email").isEmail(),
+    body("user.password").isString(),
+  ],
+  authenticationController.createUser
+);
 
 export default router;
