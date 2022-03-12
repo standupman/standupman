b84a58678c18d6ed108c6cbf1784c70d0fc3ea08
Green-Ranger11 <junior.eteuati@gmail.com>
2022-03-13 11:32:11 +1300

    Add 403 error response for existing user

--- Controllers/AuthenticationController.js
+++ Controllers/AuthenticationController.js
@@ -26,19 +26,25 @@ export default {
         req.logout();
         res.redirect('/');
     },
-    createUser : (req, res) => {
+    createUser: (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        let user = req.body.user;
-        let salt = bcrypt.genSaltSync(10);
-        user.password = bcrypt.hashSync(user.password, salt);
-        User.create(user).then(user => {
-            const token = genToken(user);
-            res.status(200).json({token});
-        }).catch(errors => {
-            res.json({message: "User not created", errors:errors});
+        const email = req.body.user.email;
+        User.findOne({ email }).then((user) => {
+            if (user) {
+                return res.status(403).json({ error: 'Email is already in use'});
+            }
+            let newUser = req.body.user;
+            let salt = bcrypt.genSaltSync(10);
+            newUser.password = bcrypt.hashSync(newUser.password, salt);
+            User.create(newUser).then(user => {
+                const token = genToken(user);
+                res.status(200).json({token});
+            }).catch(errors => {
+                res.json({message: "User not created", errors:errors});
+            });
         });
      }
 };
